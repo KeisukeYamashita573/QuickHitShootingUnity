7b0c8d8d69abe4f743c6a08596d23e9bc4ddbdd1
Nosuriba@11 <kitagawa_taiko@icloud.com>
2020-06-22 15:17:50 +0900

    とりあえず更新。

---
+++ QuickHitShooting/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    protected int _dispTime;      // 表示時間
+    protected int _banishTime;    // 消滅時間
+
+    protected int _point;       // 得点
+
+    protected const int _deathTime = 30;     // 死亡してから消えるまでの時間
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (_dispTime <= 0)
+        {
+            --_banishTime;
+        }
+        else
+        {
+            --_dispTime;
+        }
+    }
+
+    // 弾と敵が当たったかの判定
+    protected virtual void OnCollisionEnter2D(Collision2D collision)
+    {
+        /// ターゲティングの座標とマウスをクリックしたかの情報が必要
+    }
+
+    // 得点取得用
+    public int GetPoint
+    {
+        get { return _point; }
+    }
+
+    // 座標取得用
+    public Vector2 GetPos
+    {
+        get { return new Vector2(this.transform.position.x, this.transform.position.y); }
+    }
+}
---
+++ QuickHitShooting/Assets/Scripts/Enemy/SpecialEnemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialEnemy : Enemy
+{
+    public SpecialEnemy(TargetData tData)
+    {
+        /// ステータスの初期化
+        _dispTime               = tData.dispTime;
+        _banishTime             = tData.banishTime;
+        this.transform.position = new Vector3(tData.pos.x, tData.pos.y, 0);
+    }
+
+    void Start()
+    {
+        /// 得点の初期化
+        _point = 60;
+    }
+
+    void Update()
+    {
+        if (_dispTime <= 0)
+        {
+            --_banishTime;
+        }
+        else
+        {
+            --_dispTime;
+        }
+    }
+
+    protected override void OnCollisionEnter2D(Collision2D collision)
+    {
+        /// ターゲティングの座標とマウスをクリックしたかの情報が必要
+    }
+}
