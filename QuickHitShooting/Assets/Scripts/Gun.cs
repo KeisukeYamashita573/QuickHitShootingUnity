dd8db777ea4ff2dd2f7736dec486f105c7a02c24
Nosuriba@11 <kitagawa_taiko@icloud.com>
2020-07-10 13:36:44 +0900

    銃のステータス設定用スクリプトの追加を行い、使いたい銃を選択した時に ログを出すようにした。

---
+++ QuickHitShooting/Assets/Scripts/Gun.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 銃のステータス
+[System.Serializable]
+public struct GunStatus
+{
+    [Header("銃の名前")]
+    public string name;
+    [Header("マガジン内の残り弾数")]
+    public int bulletsInMagazine;
+    [Header("残り弾数")]
+    public int remainingBullets;
+    [Header("最大弾数")]
+    public int maxBullets;
+    [Header("マガジン内の最大弾数")]
+    public int maxBulletsInMagazine;
+}
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField]
+    GunStatus _gun;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 銃のステータス取得用
+    public GunStatus GetGunStatus
+    {
+        get { return _gun; }
+    }
+
+    //　銃が選択された時に入る処理
+    public void OnClick()
+    {
+        Debug.Log(_gun.name + " の銃が選択されたよ");
+    }
+}
